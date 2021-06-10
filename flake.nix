0ea8178a8e81e0763c08319247acf8d4b8f39f12
Nathaniel Griswold <nategriswold@gmail.com>
2021-06-09 21:30:03 -0500

    working build

--- flake.nix
+++ flake.nix
@@ -2,17 +2,23 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/release-20.09;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+#    flake-utils.lib.eachDefaultSystem (system:
+    let
+      system = "x86_64-linux";
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        packages.hello = pkgs.hello;
+        packages.x86_64-linux.hello = pkgs.hello;
+        packages.x86_64-linux.nixos-systemd-nspawn = import ./nixos-systemd-nspawn.nix {
+          inherit nixpkgs;
+          inherit system;
+        };
+        
         defaultPackage = self.packages.${system}.hello;
       }
-    );
+    ;
 }
---
+++ nixos-systemd-nspawn.nix
@@ -0,0 +1,40 @@
+{ nixpkgs, system }:
+let
+  myModule = ({ pkgs, ... }: {
+    time.timeZone = "America/Chicago";
+    system.stateVersion = "21.05";
+    boot.enableContainers = true;
+    systemd.services."container@" = {
+      # the start script fails to touch these if they are broken symlinks
+      preStart = ''
+                if [ -d $root ]
+                then
+                  rm $root/etc/{os-release,machine-id}
+                fi
+              '';
+    };
+    networking.nat = {
+      enable = true;
+      internalInterfaces = ["ve-+"];
+      externalInterface = "enp2s0";
+    };            
+  });
+
+  nixos = nixpkgs.lib.nixosSystem {
+    inherit system;
+    modules = [ "${nixpkgs}/nixos/modules/virtualisation/docker-image.nix" myModule ];
+  };
+  
+  showAttrs = name: value: "${name}";
+  blah = builtins.trace (toString (nixpkgs.lib.mapAttrsToList showAttrs nixos.pkgs)) "hello";
+in
+with import nixpkgs { inherit system; };
+stdenv.mkDerivation {
+  name = "nixos-systemd-nspawn";
+  unpackPhase = ":";
+  installPhase = ''
+      mkdir -p $out/etc/systemd/system
+      echo "${nixos.config.system.build.toplevel}/etc/systemd/system/{nat,container@}.service"
+      ln -s ${nixos.config.system.build.toplevel}/etc/systemd/system/{nat,container@}.service $out/etc/systemd/system/
+  '';
+}
